0959db4b0c38e532ea7887f60bf1c109c3d24f25
warscodev <warscodev@gmail.com>
2021-05-27 05:16:08 +0900

    stop.sh 오타수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,6 +5,8 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
+IDLE_PORT=$(find_idle_port)
+
 echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 PID 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
