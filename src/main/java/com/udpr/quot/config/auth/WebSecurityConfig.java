79c2bafd69d68560d11836b8bdf3b88f5b98a4a9
warscodev <warscodev@gmail.com>
2021-05-27 03:20:41 +0900

    profile 컨트롤러 생성 및 테스트 완료

--- src/main/java/com/udpr/quot/config/auth/WebSecurityConfig.java
+++ src/main/java/com/udpr/quot/config/auth/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/login", "/api/**").permitAll() // 누구나 접근 허용
+                    .antMatchers("/login", "/api/**", "/profile").permitAll() // 누구나 접근 허용
                     //.antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
                     .antMatchers("/**").hasRole("ADMIN") // ADMIN만 접근 가능
                     .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
