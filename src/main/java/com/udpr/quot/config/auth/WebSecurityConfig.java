a1391e33bd65f718eac151ee380ba8685c93b8d7
warsco <warscodev@gmail.com>
2021-08-29 03:26:17 +0900

    시큐리티 포트 리다이렉트 설정(requiresChannel)

--- src/main/java/com/udpr/quot/config/auth/WebSecurityConfig.java
+++ src/main/java/com/udpr/quot/config/auth/WebSecurityConfig.java
@@ -32,8 +32,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
 
                 .and()
-                    .portMapper()
-                    .http(8080).mapsTo(443)
+                    .requiresChannel()
+                    .antMatchers("/login*").requiresSecure()
 
 
                 .and()
