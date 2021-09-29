573cd97797aff4ace4421aa2c5c19db1cae38429
warsco <warscodev@gmail.com>
2021-09-30 04:42:31 +0900

    xss 필터 수정(lucy-xss 제거 및 json 필터 수정)

--- src/main/java/com/udpr/quot/config/HtmlCharacterEscapes.java
+++ src/main/java/com/udpr/quot/config/HtmlCharacterEscapes.java
@@ -4,12 +4,28 @@ import com.fasterxml.jackson.core.SerializableString;
 import com.fasterxml.jackson.core.io.CharacterEscapes;
 import com.fasterxml.jackson.core.io.SerializedString;
 import org.apache.commons.text.StringEscapeUtils;
+import org.apache.commons.text.translate.AggregateTranslator;
+import org.apache.commons.text.translate.CharSequenceTranslator;
+import org.apache.commons.text.translate.EntityArrays;
+import org.apache.commons.text.translate.LookupTranslator;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class HtmlCharacterEscapes extends CharacterEscapes {
 
+    private static final long serialVersionUID = 20210929L;
     private final int[] asciiEscapes;
+    private final CharSequenceTranslator translator;
 
     public HtmlCharacterEscapes() {
+
+        /*Map<CharSequence, CharSequence> customMap = new HashMap<>();
+        customMap.put("(", "&#40;");
+        Map<CharSequence, CharSequence> CUSTOM_ESCAPE = Collections.unmodifiableMap(customMap);*/
+
         // 1. XSS 방지 처리할 특수 문자 지정
         asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
         asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
@@ -20,6 +36,14 @@ public class HtmlCharacterEscapes extends CharacterEscapes {
         asciiEscapes[')'] = CharacterEscapes.ESCAPE_CUSTOM;
         asciiEscapes['#'] = CharacterEscapes.ESCAPE_CUSTOM;
         asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
+
+        // XSS 방지 처리 특수 문자 인코딩 값 지정
+        translator = new AggregateTranslator(
+                new LookupTranslator(EntityArrays.BASIC_ESCAPE),  // <, >, &, " 는 여기에 포함됨
+                new LookupTranslator(EntityArrays.ISO8859_1_ESCAPE),
+                new LookupTranslator(EntityArrays.HTML40_EXTENDED_ESCAPE)
+                /*new LookupTranslator(CUSTOM_ESCAPE)*/
+        );
     }
 
     @Override
@@ -29,16 +53,10 @@ public class HtmlCharacterEscapes extends CharacterEscapes {
 
     @Override
     public SerializableString getEscapeSequence(int ch) {
-        SerializedString serializedString;
-        char charAt = (char) ch;
-        if (Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt)) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("\\u");
-            sb.append(String.format("%04x", ch));
-            serializedString = new SerializedString(sb.toString());
-        } else {
-            serializedString = new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString(charAt)));
-        }
-        return serializedString;
+        /*return new SerializedString(translator.translate(Character.toString((char) ch)));*/
+        return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
+
     }
+
+
 }
--- src/main/java/com/udpr/quot/config/WebConfig.java
+++ src/main/java/com/udpr/quot/config/WebConfig.java
@@ -1,13 +1,11 @@
 package com.udpr.quot.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 import com.udpr.quot.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,36 +14,43 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+    public class WebConfig implements WebMvcConfigurer{
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
     private final ObjectMapper objectMapper;
-
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginUserArgumentResolver);
     }
 
-    /*
-     * lucy-xss-filter
-     *
-     * */
+
     @Bean
-    public FilterRegistrationBean<XssEscapeServletFilter> getFilterRegistrationBean() {
-        FilterRegistrationBean<XssEscapeServletFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new XssEscapeServletFilter());
-        registrationBean.setOrder(1);
-        registrationBean.addUrlPatterns("/*");
-        return registrationBean;
+    public HttpMessageConverter escapingConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+        /*objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);*/
+
+        MappingJackson2HttpMessageConverter escapingConverter =
+                new MappingJackson2HttpMessageConverter();
+        escapingConverter.setObjectMapper(objectMapper);
+
+        return escapingConverter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(escapingConverter());
     }
 
     @Bean
     public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
-        // MappingJackson2HttpMessageConverter Default ObjectMapper 설정 및 ObjectMapper Config 설정
-        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
-        objectMapper.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
-        return new MappingJackson2HttpMessageConverter(objectMapper);
+        ObjectMapper copy = objectMapper.copy();
+        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(copy);
     }
+
 
 }
