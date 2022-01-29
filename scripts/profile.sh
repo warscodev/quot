d43aefac4d7bcee702e48298433ca9c3d7e27781
warsco <warscodev@gmail.com>
2022-01-30 02:25:44 +0900

    배포 테스트

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,25 +5,18 @@ function find_idle_profile()
 {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/_profile)
 
-    echo "RESPONSE_CODE : $RESPONSE_CODE"
-
     if [ "${RESPONSE_CODE}" -ge 400 ]
 
     then
-      echo " then - CURRENT_PROFILE : real2"
       CURRENT_PROFILE=real2
     else
-      echo "else - CURRENT_PROFILE : (curl -s http://localhost/_profile)"
       CURRENT_PROFILE=$(curl -s http://localhost/_profile)
-      echo "CURRENT_PROFILE : ${CURRENT_PROFILE}"
     fi
 
     if [ "${CURRENT_PROFILE}" == real1 ]
     then
-      echo "IDLE_PROFILE : real2"
       IDLE_PROFILE=real2
     else
-      echo "IDLE_PROFILE : real1"
       IDLE_PROFILE=real1
     fi
 
@@ -33,9 +26,7 @@ function find_idle_profile()
 # 쉬고 있는 PROFILE의 PORT 찾기
 function find_idle_port()
 {
-    echo " > find_idle_port 실행"
     IDLE_PROFILE=$(find_idle_profile)
-    echo " > IDLE_PROFILE on find_idle_port : $IDLE_PROFILE"
     if [ "${IDLE_PROFILE}" == real1 ]
     then
       echo "8081"
