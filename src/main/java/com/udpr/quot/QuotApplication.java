6a9b959e3aa08565379275a9d3158d5150194492
warsco <warscodev@gmail.com>
2022-01-31 07:49:45 +0900

    ec2 에러로그 제거(엔드포인트 수정)

--- src/main/java/com/udpr/quot/QuotApplication.java
+++ src/main/java/com/udpr/quot/QuotApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+		}
+)
 public class QuotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuotApplication.class, args);
 	}
-
 }
